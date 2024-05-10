de28fded63e08db99b7eacc4f31823b94dea3b16
Almir Rogério Camolesi <camolesi@fema.edu.br>
2024-05-10 20:59:58 -0300

    Projeto Atualizado com CORS

--- Program.cs
+++ Program.cs
@@ -10,6 +10,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<Contexto>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("conexao")));
 
+//permitir cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("https://localhost:7271");
+                      });
+});
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
